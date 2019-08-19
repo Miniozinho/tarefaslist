ef3bfa076b3edbd7b9488c8854703b2c0fd28f7d
Gabriel Erminio Machado <miniozinho@gmail.com>
2019-08-18 22:28:20 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tarefaslist/create.java
@@ -0,0 +1,14 @@
+package com.example.tarefaslist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create);
+    }
+
+}
